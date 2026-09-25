8a06e01e38daf62e24e41fb3e02c1ea849434a6c
0xdaruby <Edaraiquaibom16@gmail.com>
2026-09-25 18:20:07 +0100

    Sitemap Update

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,13 @@ export default defineConfig({
   plugins: [
     react(),
     Sitemap({
-      hostname: "https://0xdaruby.vercel.app",
-      dynamicRoutes: ["/"],
+      hostname: "https://oxdaruby.tech",
+      exclude: [
+        "/BingSiteAuth.xml",
+        "/f4536413c891408e8f7064b1313af463.txt",
+        "/google10ae71c7925a91bc.html",
+        "/google10ae71c7925a91bc",
+      ],
     }),
   ],
 });
